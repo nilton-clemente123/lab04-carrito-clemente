77fa2a944f6d9e77d29f2246d6f17fe01ba6c17f
nilton-clemente123 <nilton.clemente@tecsup.edu.pe>
2026-09-16 14:48:09 -0500

    Avance: LazyColumn inicial

--- app/src/main/java/com/tecsup/lab04carritotecsup/MainActivity.kt
+++ app/src/main/java/com/tecsup/lab04carritotecsup/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,12 +13,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,6 +34,7 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -34,13 +43,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tecsup.lab04carritotecsup.ui.theme.Lab04CarritoTecsupTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Lab04CarritoTecsupTheme {
-
+                PantallaRegistro()
             }
         }
     }
@@ -135,6 +145,22 @@ fun PantallaRegistro(modifier: Modifier = Modifier) {
                 text = "Productos: ${productos.size}"
             )
 
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                items(productos) { producto ->
+                    TarjetaProducto(
+                        producto = producto,
+                        onEliminar = { productos.remove(producto) }
+                    )
+                }
+            }
+
+
+
             Spacer(modifier = Modifier.height(24.dp))
             if (mostrarResumen) {
                 val precioNum = precio.toDoubleOrNull() ?: 0.0
@@ -192,5 +218,32 @@ fun PantallaRegistro(modifier: Modifier = Modifier) {
 
 
 
+}
 
+@Composable
+fun TarjetaProducto(producto: Producto, onEliminar: () -> Unit) {
+    Card(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier.padding(16.dp).fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+// TODO: Text nombre (titleMedium, negrita)
+                Text("NOMBRE: ${producto.nombre}")
+// TODO: Text "S/ precio x cantidad" (gris)
+                Text(" S/. ${producto.precio * producto.cantidad}")
+            }
+// TODO: Text del importe (precio x cantidad, 2 decimales)
+            Text(
+                "Importe: S/ ${"%.2f".format(producto.precio * producto.cantidad)}"
+            )
+            IconButton(onClick = onEliminar) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Eliminar",
+                    tint = MaterialTheme.colorScheme.error
+                )
+            }
+        }
+    }
 }
